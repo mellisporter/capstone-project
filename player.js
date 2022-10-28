5adab45d0322ae1022b6759f27981d8c256e9d0c
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-28 11:24:07 -0400

    player created

---
+++ player.js
@@ -0,0 +1,16 @@
+export class Player {
+    constructor(game) {
+        this.game = game;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = 0;
+        this.y = 100;
+    }
+    update() {
+
+    }
+    draw(context) {
+        context.fillStyle = 'red';
+        context.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
